c073e1c2d224322b64289e8e21abe85c193e063f
Michael Rhodes <michael@byu430228wks.app.byu.edu>
2016-02-01 19:41:45 -0700

    ModelPopulator initial commit

---
+++ src/client/communication/ModelPopulator.java
@@ -0,0 +1,17 @@
+package client.communication;
+
+import com.google.gson.Gson;
+import org.json.JSONObject;
+import shared.communication.fromServer.game.ClientModel;
+
+/**
+ * Michael Rhodes
+ * CS 340
+ * Section 2
+ * Team 10
+ */
+public class ModelPopulator {
+    public static void populateModel(JSONObject o) {
+
+    }
+}
