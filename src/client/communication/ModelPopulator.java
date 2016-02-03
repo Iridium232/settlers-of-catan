97e3826f6f088fa7618aae7b0a094cdada0d03c3
Michael Rhodes <michael@Michaels-MacBook-Pro-3.local>
2016-02-02 23:41:45 -0700

    finish ModelPopulator

--- src/client/communication/ModelPopulator.java
+++ src/client/communication/ModelPopulator.java
@@ -4,10 +4,16 @@ import com.google.gson.Gson;
 import org.json.JSONObject;
 import shared.communication.ResourceList;
 import shared.communication.fromServer.game.*;
+import shared.communication.fromServer.game.DevCardList;
 import shared.communication.fromServer.game.EdgeLocation;
 import shared.communication.fromServer.game.Port;
 import shared.communication.fromServer.game.Road;
+import shared.communication.fromServer.game.Player;
+import shared.communication.fromServer.game.TradeOffer;
+import shared.communication.fromServer.game.TurnTracker;
+import shared.definitions.CatanColor;
 import shared.definitions.HexType;
+import shared.definitions.TurnStatus;
 import shared.locations.*;
 import shared.model.*;
 import shared.model.ports.*;
@@ -19,12 +25,13 @@ import shared.model.ports.*;
  * Team 10
  */
 public class ModelPopulator {
-    public static void populateModel(JSONObject o) {
+    public static void populateModel(JSONObject o, Fascade fascade) {
         ModelPopulator mp = new ModelPopulator();
         Gson gson = new Gson();
         ServerModel serverModel = gson.fromJson(o.toString(), ServerModel.class);
         Game newModel = new Game();
         mp.populateGame(serverModel, newModel);
+        //fascade.attachNewModel(newModel);
     }
 
     private ModelPopulator() {
@@ -35,6 +42,11 @@ public class ModelPopulator {
         populateChat(serverModel, newModel);
         populateLog(serverModel, newModel);
         populateMap(serverModel, newModel);
+        populatePlayers(serverModel, newModel);
+        populateTradeOffer(serverModel, newModel);
+        populateTurnTracker(serverModel, newModel);
+        newModel.setVersion(serverModel.getVersion());
+        newModel.setWinner(serverModel.getWinner());
     }
 
     private void populateBank(ServerModel serverModel, Game newModel) {
@@ -73,6 +85,10 @@ public class ModelPopulator {
         populateHexes(serverMap, clientMap);
         populatePorts(serverMap, clientMap);
         populateRoads(serverMap, clientMap);
+        populateSettlements(serverMap, clientMap);
+        populateCities(serverMap, clientMap);
+        clientMap.setRadius(serverMap.getRadius());
+        populateRobber(serverMap, clientMap);
     }
 
     private void populateHexes(Map serverMap, GameMap clientMap) {
@@ -202,5 +218,135 @@ public class ModelPopulator {
             } catch (Exception e) {
             }
         }
+    }
+
+    private void populateCities(Map serverMap, GameMap clientMap) {
+        VertexObject[] serverCities = serverMap.getCities();
+
+        for (VertexObject vertexObject : serverCities) {
+            int serverOwner = vertexObject.getOwner();
+            VertexLocation serverVertexLocation = vertexObject.getLocation();
+            HexLocation serverHexLocation = serverVertexLocation.getHexLoc();
+
+            VertexLocation newLocation = new VertexLocation(new HexLocation(serverHexLocation.getX(),
+                    serverHexLocation.getY()), serverVertexLocation.getDir());
+
+            City newCity = new City();
+            newCity.setLocation(newLocation);
+            newCity.setOwner(serverOwner);
+            try {
+                clientMap.addBuilding(newCity);
+            } catch (Exception e) {
+            }
+        }
+    }
+
+    private void populateRobber(Map serverMap, GameMap clientMap) {
+        HexLocation serverLocation = serverMap.getRobber();
+        clientMap.getRobber().setLocation(new HexLocation(serverLocation.getX(), serverLocation.getY()));
+    }
+
+    private void populatePlayers(ServerModel serverModel, Game newModel) {
+        Player[] serverPlayers = serverModel.getPlayers();
+
+        for (Player player : serverPlayers) {
+            shared.model.Player newPlayer = new shared.model.Player();
+            newPlayer.setCities(player.getCities());
+            newPlayer.setColor(player.getColor());
+            newPlayer.setDiscarded(player.isDiscarded());
+            newPlayer.setMonuments(player.getMonuments());
+            newPlayer.setName(player.getName());
+            newPlayer.setPlayerID(player.getPlayerID());
+            newPlayer.setPlayerIndex(player.getPlayerIndex());
+            newPlayer.setPlayedDevCard(player.isPlayedDevCard());
+            newPlayer.setRoads(player.getRoads());
+            newPlayer.setSettlements(player.getSettlements());
+            newPlayer.setSoldiers(player.getSoldiers());
+            newPlayer.setVictoryPoints(player.getVictoryPoints());
+
+            ResourceList serverList = player.getResources();
+            ResourceMultiSet newMultiSet = new ResourceMultiSet();
+            populateResourceMultiSet(serverList, newMultiSet);
+
+            DevCardList serverOldList = player.getOldDevCards();
+            shared.model.DevCardList newOldList = new shared.model.DevCardList();
+            populateDevCardList(serverOldList, newOldList);
+
+            DevCardList serverNewList = player.getNewDevCards();
+            shared.model.DevCardList newNewList = new shared.model.DevCardList();
+            populateDevCardList(serverNewList, newNewList);
+
+            newPlayer.setResources(newMultiSet);
+            newPlayer.setOldDevCards(newOldList);
+            newPlayer.setNewDevCards(newNewList);
+            newModel.setPlayer(newPlayer, player.getPlayerIndex());
+        }
+        setPieceColors(newModel);
+    }
+
+    private void setPieceColors(Game newModel) {
+        for (shared.model.Road road : newModel.getMap().getRoads()) {
+            for (shared.model.Player player : newModel.getPlayers()) {
+                if (road.getOwner() == player.getPlayerIndex()) {
+                    if (player.getColor().equals("red")) { road.setColor(CatanColor.RED); }
+                    if (player.getColor().equals("orange")) { road.setColor(CatanColor.ORANGE); }
+                    if (player.getColor().equals("yellow")) { road.setColor(CatanColor.YELLOW); }
+                    if (player.getColor().equals("blue")) { road.setColor(CatanColor.BLUE); }
+                    if (player.getColor().equals("green")) { road.setColor(CatanColor.GREEN); }
+                    if (player.getColor().equals("purple")) { road.setColor(CatanColor.PURPLE); }
+                    if (player.getColor().equals("puce")) { road.setColor(CatanColor.PUCE); }
+                    if (player.getColor().equals("white")) { road.setColor(CatanColor.WHITE); }
+                    if (player.getColor().equals("brown")) { road.setColor(CatanColor.BROWN); }
+                }
+            }
+        }
+    }
+
+    private void populateResourceMultiSet(ResourceList serverList, ResourceMultiSet newSet) {
+        newSet.setBrick(serverList.getBrick());
+        newSet.setOre(serverList.getOre());
+        newSet.setSheep(serverList.getSheep());
+        newSet.setWheat(serverList.getWheat());
+        newSet.setWood(serverList.getWood());
+    }
+
+    private void populateDevCardList(DevCardList serverList, shared.model.DevCardList newList) {
+        newList.setMonopoly(serverList.getMonopoly());
+        newList.setMonument(serverList.getMonument());
+        newList.setRoad_building(serverList.getRoadBuilding());
+        newList.setSoldier(serverList.getSoldier());
+        newList.setYear_of_plenty(serverList.getYearOfPlenty());
+    }
+
+    private void populateTradeOffer(ServerModel serverModel, Game newModel) {
+        TradeOffer serverOffer = serverModel.getTradeOffer();
+        shared.model.TradeOffer newOffer = new shared.model.TradeOffer();
+
+        newOffer.setSender(serverOffer.getSender());
+        newOffer.setReciever(serverOffer.getReceiver());
+
+        ResourceMultiSet newMultiSet = new ResourceMultiSet();
+        populateResourceMultiSet(serverOffer.getOffer(), newMultiSet);
+        newOffer.setOffer(newMultiSet);
+
+        newModel.setTrade_offer(newOffer);
+    }
+
+    private void populateTurnTracker(ServerModel serverModel, Game newModel) {
+        TurnTracker serverTracker = serverModel.getTurnTracker();
+        shared.model.TurnTracker newTracker = new shared.model.TurnTracker();
+
+        newTracker.setActive_player(serverTracker.getCurrentTurn());
+        newTracker.setLargest_army_player(serverTracker.getLargestArmy());
+        newTracker.setLongest_road_player(serverTracker.getLongestRoad());
+
+        if (serverTracker.getStatus().equals("FirstRound")) { newTracker.setStatus(TurnStatus.FIRSTROUND); }
+        if (serverTracker.getStatus().equals("SecondRound")) { newTracker.setStatus(TurnStatus.SECONDROUND); }
+        if (serverTracker.getStatus().equals("Rolling")) { newTracker.setStatus(TurnStatus.ROLLING); }
+        if (serverTracker.getStatus().equals("Robbing")) { newTracker.setStatus(TurnStatus.ROBBING); }
+        if (serverTracker.getStatus().equals("Discarding")) { newTracker.setStatus(TurnStatus.DISCARDING); }
+        if (serverTracker.getStatus().equals("Playing")) { newTracker.setStatus(TurnStatus.PLAYING); }
+
+        newModel.setTurn_tracker(newTracker);
     }
 }
