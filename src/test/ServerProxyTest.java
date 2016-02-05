2f322baf6ddcc5fabe57377fa31a8395529c958d
awerp <johnsondouglas918@gmail.com>
2016-02-05 13:34:09 -0700

    ServerTest
    first test

--- src/test/ServerProxyTest.java
+++ src/test/ServerProxyTest.java
@@ -2,23 +2,49 @@ package test;
 
 import static org.junit.Assert.*;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.BeforeClass;
 import org.junit.Test;
+
+import client.communication.ClientCommunicator;
+import client.communication.ModelPopulator;
+import client.communication.ServerProxy;
+import shared.model.Fascade;
 
 public class ServerProxyTest {
 
-	@Test
-	public void testServerProxy() {
-		fail("Not yet implemented");
+	private static ServerProxy sp;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+		Fascade f=new Fascade();
+		JSONObject json;
+		sp=new ServerProxy("localhost",8081,f);
+		ClientCommunicator.getSingleton("localhost", "8081");
+		try 
+		{
+			 json = new JSONObject(SetupModel.model1);
+		} 
+		catch (JSONException e) 
+		{
+			e.printStackTrace();
+			return;
+		}
+		ModelPopulator.populateModel(json,f);
+		
 	}
 
 	@Test
 	public void testLogin() {
-		fail("Not yet implemented");
+		String result=sp.login("bob", "bob");
+		System.out.println(result);
+		assertTrue (result=="Success");
 	}
-
+	
 	@Test
 	public void testRegister() {
-		fail("Not yet implemented");
+		String result=sp.login("doug", "bob");
+		assertTrue(result=="Success");
 	}
 
 	@Test
