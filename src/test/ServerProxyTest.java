9677c12d5c845d51ee9b16941458c8d1cd22cd70
awerp <johnsondouglas918@gmail.com>
2016-02-05 19:19:36 -0700

    Server Proxy tests
    Adding a list of commands that can be sent to the server to set up the
    game where we want it.

--- src/test/ServerProxyTest.java
+++ src/test/ServerProxyTest.java
@@ -2,7 +2,12 @@ package test;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,46 +32,48 @@ public class ServerProxyTest {
 		JSONObject json;
 		sp=new ServerProxy("localhost",8081,f);
 		ClientCommunicator.getSingleton("localhost", "8081");
-		try 
-		{
-			 json = new JSONObject(SetupModel.model1);
-		} 
-		catch (JSONException e) 
-		{
-			e.printStackTrace();
-			return;
-		}
-		ModelPopulator.populateModel(json,f);
+//		try 
+//		{
+//			 json = new JSONObject(SetupModel.model1);
+//		} 
+//		catch (JSONException e) 
+//		{
+//			e.printStackTrace();
+//			return;
+//		}
+//		ModelPopulator.populateModel(json,f);
 	//	sp.register("bob", "bob");
 	}
 	
 	@Test
 	public void testLogin() {
-		String result=sp.login("bob", "bob");
+		String result=sp.login("Pete", "pete");
 		assertEquals (result,"200");
 	}
-	
+	@Ignore
 	@Test
 	public void testRegister() {
 		String result=sp.register("greg", "bob");
 		System.out.println(result);
 		assertEquals("200",result);
 	}
-
+	@Ignore
 	@Test
 	public void testGetGameList() {
 		List<Game> result=sp.getGameList();
 		assert(!result.isEmpty());
 	}
-
+	@Ignore
 	@Test
-	public void testCreateGame() {
+	public void testCreateandJoinGame() {
 		Game test=sp.createGame("testGame", false, false, false);
 		assert(test.getPlayers().length==0);
 		assert(test.getTitle()=="testGame");
 		assert(test.getId()!=0);
+		String testy=sp.joinGame("Bob", 3, CatanColor.RED);
+		
 	}
-
+	@Ignore
 	@Test
 	public void testJoinGame() {
 		String test=sp.joinGame("Bob", 3, CatanColor.RED);
@@ -75,32 +82,7 @@ public class ServerProxyTest {
 	}
 
 	@Test
-	public void testSaveGame() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testLoadGame() {
-		fail("Not yet implemented");
-	}
-
-	@Test
 	public void testGetModel() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testReset() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetCommands() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testExecuteCommands() {
 		fail("Not yet implemented");
 	}
 
@@ -173,9 +155,34 @@ public class ServerProxyTest {
 	public void testMonopoly() {
 		fail("Not yet implemented");
 	}
-
+	@Ignore
 	@Test
 	public void testMonument() {
+		FileReader fr;
+		StringBuilder sb=new StringBuilder();
+		String line;
+		try {
+			fr = new FileReader("commands.txt");
+			BufferedReader br=new BufferedReader(fr);
+			while((line=br.readLine())!=null){
+				sb.append(line);
+			}
+		} catch (FileNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		try {
+			sp.login("Pete", "pete");
+			sp.joinGame("pete", 0, CatanColor.RED);
+			ClientCommunicator.getSINGLETON().doPost("/game/commands", sb.toString());
+			System.out.print(sp.monument().toString());
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		fail("Not yet implemented");
 	}
 
