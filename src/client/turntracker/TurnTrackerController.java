f967c75e4996e61e480e40ff63338aa90c5abdbd
Michael <mjrhodes22@gmail.com>
2016-02-25 12:00:09 -0700

    turnTrackerController completed

--- src/client/turntracker/TurnTrackerController.java
+++ src/client/turntracker/TurnTrackerController.java
@@ -1,9 +1,13 @@
 package client.turntracker;
 
 import client.control.IObserver;
+import client.control.Reference;
 import shared.definitions.CatanColor;
 import client.base.*;
+import shared.definitions.TurnStatus;
 import shared.model.Fascade;
+import shared.model.Game;
+import shared.model.player.Player;
 
 import javax.xml.parsers.FactoryConfigurationError;
 
@@ -13,9 +17,13 @@ import javax.xml.parsers.FactoryConfigurationError;
  */
 public class TurnTrackerController extends Controller implements ITurnTrackerController, IObserver {
 
+	boolean playerInitialized;
+
 	public TurnTrackerController(ITurnTrackerView view) {
 		
 		super(view);
+
+		playerInitialized = false;
 		
 		initFromModel();
 	}
@@ -39,7 +47,71 @@ public class TurnTrackerController extends Controller implements ITurnTrackerCon
 
 	@Override
 	public void ObservableChanged() {
+		Reference r = Reference.GET_SINGLETON();
+		Game model = r.getFascade().getModel();
+		if (!playerInitialized) {
+			getView().initializePlayer(r.getPlayer_index(), r.getName(), r.getPlayer_color());
+			getView().setLocalPlayerColor(r.getPlayer_color());
+			playerInitialized = true;
+		}
+		Player localPlayer = null;
+		for (Player player : model.getPlayers()) {
+			if (player.getPlayerIndex() == r.getPlayer_index()) {
+				localPlayer = player;
+			}
+		}
 
+		if (localPlayer == null) { return; }
+
+		boolean highlight = false;
+		if (model.getTurn_tracker().getActive_player() == localPlayer.getPlayerIndex()) {
+			highlight = true;
+		}
+
+		boolean largestArmy = false;
+		if (model.getTurn_tracker().getLargest_army_player() == localPlayer.getPlayerIndex()) {
+			largestArmy = true;
+		}
+
+		boolean longestRoad = false;
+		if (model.getTurn_tracker().getLongest_road_player() == localPlayer.getPlayerIndex()) {
+			longestRoad = true;
+		}
+
+		getView().updatePlayer(localPlayer.getPlayerIndex(), localPlayer.getVictoryPoints(),
+				highlight, largestArmy, longestRoad);
+
+		String label = "";
+		boolean enable = false;
+		TurnStatus state = model.getTurnStatus(localPlayer.getPlayerIndex());
+		switch (state) {
+			case ROLLING:
+				label = "Roll the Dice";
+				break;
+			case ROBBING:
+				label = "Rob a Player";
+				break;
+			case PLAYING:
+				label = "Finish Turn";
+				enable = true;
+				break;
+			case DISCARDING:
+				label = "Discard Cards";
+				break;
+			case FIRSTROUND:
+				label = "Place Structures";
+				break;
+			case SECONDROUND:
+				label = "Place Structures";
+				break;
+			case WAITING:
+				label = "Waiting for Players";
+				break;
+			case TRADING:
+				label = "Trading";
+				break;
+		}
+		getView().updateGameState(label, enable);
 	}
 }
 
