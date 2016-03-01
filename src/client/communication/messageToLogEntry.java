2320d177eabcd058a7d372b6ec631db1e5dac1a9
awerp <johnsondouglas918@gmail.com>
2016-02-29 22:33:40 -0700

    rename

--- src/client/communication/messageToLogEntry.java
+++ src/client/communication/messageToLogEntry.java
@@ -5,12 +5,12 @@ import shared.definitions.CatanColor;
 import shared.model.messages.MessageLine;
 import shared.model.player.Player;
 
-public class messageToLogEntry {
-	private static messageToLogEntry single;
-	private messageToLogEntry(){}
-	public messageToLogEntry getMessageToLog(){
+public class MessageToLogEntry {
+	private static MessageToLogEntry single;
+	private MessageToLogEntry(){}
+	public static MessageToLogEntry getMessageToLog(){
 		if(single==null){
-			single=new messageToLogEntry();
+			single=new MessageToLogEntry();
 			return single;
 		}else{
 			return single;
@@ -19,33 +19,32 @@ public class messageToLogEntry {
 	public LogEntry convert(MessageLine line){
 		LogEntry special=null;
 		for(Player p:Reference.GET_SINGLETON().getFascade().getModel().getPlayers()){
-			if(line.getSource()==p.getName()){
+			if(line.getSource().equals(p.getName())){
 				special=new LogEntry(colorSwitch(p.getColor()),line.getMessage());
 			}
 		}
 		return special;
 	}
 	
-	private CatanColor colorSwitch(String color){
-		if(color=="red"){
+	private static CatanColor colorSwitch(String color){
+		if(color.equals("red")){
 			return CatanColor.RED;				
-		} else if(color=="orange"){
+		} else if(color.equals("orange")){
 			return CatanColor.ORANGE;
-		} else if (color=="yellow"){
+		} else if (color.equals("yellow")){
 			return CatanColor.YELLOW;
-		} else if (color=="blue"){
+		} else if (color.equals("blue")){
 			return CatanColor.BLUE;
-		} else if (color=="green"){
+		} else if (color.equals("green")){
 			return CatanColor.GREEN;
-		} else if (color=="purple"){
+		} else if (color.equals("purple")){
 			return CatanColor.PURPLE;
-		} else if(color =="puce"){
+		} else if(color.equals("puce")){
 			return CatanColor.PUCE;
-		} else if(color=="white"){
+		} else if(color.equals("white")){
 			return CatanColor.WHITE;
-		} else if(color=="brown"){
+		} else if(color.equals("brown")){
 			return CatanColor.BROWN;
 		} else return null;
 	}
 }
-//RED, ORANGE, YELLOW, BLUE, GREEN, PURPLE, PUCE, WHITE, BROWN
