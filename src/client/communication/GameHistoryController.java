1deb8dd99184a2079c8060069bd3d872e2d0eaca
awerp <johnsondouglas918@gmail.com>
2016-02-29 22:43:38 -0700

    Completed game history controller

--- src/client/communication/GameHistoryController.java
+++ src/client/communication/GameHistoryController.java
@@ -1,22 +1,23 @@
 package client.communication;
 
 import java.util.*;
-import java.util.List;
 
 import client.base.*;
+import client.control.IObserver;
+import client.control.Reference;
 import shared.definitions.*;
+import shared.model.messages.MessageLine;
 
 
 /**
  * Game history controller implementation
  */
-public class GameHistoryController extends Controller implements IGameHistoryController {
+public class GameHistoryController extends Controller implements IGameHistoryController, IObserver {
 
 	public GameHistoryController(IGameHistoryView view) {
 		
 		super(view);
-		
-		initFromModel();
+		Reference.GET_SINGLETON().getFascade().addObserver(this);
 	}
 	
 	@Override
@@ -28,13 +29,22 @@ public class GameHistoryController extends Controller implements IGameHistoryCon
 	 * @post The Game history display will display upto the last 8 moves that have been made
 	 */
 	private void initFromModel() {
-		
-		//<temp>
-		
+		List<LogEntry> entries = new ArrayList<>();
 
-//		getView().setEntries();
-	
-		//</temp>
+		MessageLine[] messages = Reference.GET_SINGLETON().getFascade().getLog();
+		
+		for (MessageLine messageLine : messages) {
+			entries.add(MessageToLogEntry.getMessageToLog().convert(messageLine));			
+		}
+
+		this.getView().setEntries(entries);
+
+	}
+
+	@Override
+	public void ObservableChanged() {
+		// TODO Auto-generated method stub
+		initFromModel();
 	}
 	
 }
