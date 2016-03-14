3560ea1fe6a17ec813632f71f6792c5ff6850cf3
awerp <johnsondouglas918@gmail.com>
2016-03-14 12:54:11 -0600

    Handlers

---
+++ src/server/handlers/BuildCityHandler.java
@@ -0,0 +1,20 @@
+package server.handlers;
+
+import java.io.IOException;
+
+import javax.xml.ws.spi.http.HttpExchange;
+import javax.xml.ws.spi.http.HttpHandler;
+
+public class BuildCityHandler extends HttpHandler {
+	/**
+	 * @post a build city command is created and executed on the proper coordinates with the correct owner
+	 * @author Doug
+	 *
+	 */
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/server/handlers/BuyDevCardHandler.java
@@ -0,0 +1,18 @@
+package server.handlers;
+
+import java.io.IOException;
+
+import javax.xml.ws.spi.http.HttpExchange;
+import javax.xml.ws.spi.http.HttpHandler;
+
+public class BuyDevCardHandler extends HttpHandler {
+/**
+ * @post handles a BuyDevCard request and creates a buydevcard command. 
+ */
+	@Override
+	public void handle(HttpExchange exchange) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
