7d6fea96a4cb9449b480a993c4bffa034fc8d811
Michael <mjrhodes22@gmail.com>
2016-03-18 12:25:09 -0600

    add ModelTranslator

---
+++ src/server/communication/ModelTranslator.java
@@ -0,0 +1,17 @@
+package server.communication;
+
+import shared.communication.fromServer.game.CommunicationModel;
+
+/**
+ * Michael Rhodes
+ * CS 340
+ * Section 2
+ * Team 10
+ */
+public class ModelTranslator {
+
+    public static CommunicationModel translateModel(shared.model.Game serverModel) {
+        //To do
+        return null;
+    }
+}
